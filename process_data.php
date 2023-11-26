7bf4eefec5e864041127fee4cd54da8454782e82
gahyeon <1000ji1@naver.com>
2023-11-27 02:42:58 +0900

    feat : add utf-8

--- process_data.php
+++ process_data.php
@@ -7,6 +7,7 @@ $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+mysqli_set_charset($connection, 'utf8mb4');
 
 $database = mysqli_select_db($connection, DB_DATABASE);
 
